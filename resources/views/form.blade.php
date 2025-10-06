1977be89495981aee1f50d88214c93ede1522890
NEELIMA2004GUPTA <niyati123gupta@gmail.com>
2025-10-06 18:26:41 +0530

    s

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -10,10 +10,10 @@
     <form action="{{url('/')}}/register" method='post'>
         @csrf
         <h1>Registration</h1>
-        <x-input type="text" name="name" label="Please enter your name"></x-input>
-        <x-input type="text" name="email" label="Please enter your email"></x-input>
-        <x-input type="password" name="password" label="Please enter your password"></x-input>
-        <x-input type="password" name="password_confirmation" label="Please enter your password again"></x-input>
+        <x-input type="text" name="name" label="Please enter your name"></x-input><br><br>
+        <x-input type="text" name="email" label="Please enter your email"></x-input><br><br>
+        <x-input type="password" name="password" label="Please enter your password"></x-input><br><br>
+        <x-input type="password" name="password_confirmation" label="Please enter your password again"></x-input><br><br>
         <button type="submit">Submit</button>
     </form>
 </body>
